165170cfaca1e7dacdbecbac75789ed87d1c81da
arockiamvictormark <arockiamvictormark@gmail.com>
2023-04-04 20:25:56 +0530

    ascdsc

---
+++ src/famous/legends/Sort_Ascending_Descending.java
@@ -0,0 +1,17 @@
+package famous.legends;
+
+public class Sort_Ascending_Descending {
+	
+	private static void ascending_order() {
+		int[] num = new int[] {5,2,8,7,1};
+		
+		for (int i = 0; i < num.length; i++) {
+			System.out.print(num[i]+" ");
+		}
+	}
+	
+	public static void main(String[] args) {
+		ascending_order();
+	}
+
+}
