db80548bbd3dffce2df09f30eb8b1d4e51245dbd
arockiamvictormark <arockiamvictormark@gmail.com>
2023-04-04 23:53:09 +0530

    ad

--- src/famous/legends/Sort_Ascending_Descending.java
+++ src/famous/legends/Sort_Ascending_Descending.java
@@ -1,17 +1,45 @@
 package famous.legends;
 
 public class Sort_Ascending_Descending {
+	static int num[] = {5,2,8,7,1};
 	
 	private static void ascending_order() {
-		int[] num = new int[] {5,2,8,7,1};
-		
+		int temp;		
 		for (int i = 0; i < num.length; i++) {
-			System.out.print(num[i]+" ");
+			for (int j = i+1; j < num.length; j++) {
+				if (num[i]>num[j]) {
+					temp=num[i];
+					num[i]=num[j];
+					num[j]=temp;
+				}
+			}
+		}
+		System.out.println("Ascending Order: ");
+		for (int k = 0; k < num.length; k++) {
+			System.out.print(num[k]+" ");
+		}
+	}
+	
+	private static void decending_order() {
+		int temp;
+		for (int i = 0; i < num.length; i++) {
+			for (int j = i+1; j < num.length; j++) {
+				if (num[i]<num[j]) {
+					temp=num[i];
+					num[i]=num[j];
+					num[j]=temp;
+				}
+			}
+		}
+		System.out.println("\nDesending Order: ");
+		for (int k = 0; k < num.length; k++) {
+			System.out.print(num[k]+" ");
 		}
 	}
 	
 	public static void main(String[] args) {
 		ascending_order();
+		decending_order();
 	}
 
 }
